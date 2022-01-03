e19a971db6dd87868983d9110071cde209c873e0
hagevvashi <hagevvashi@gmail.com>
2022-01-03 22:10:05 +0900

    refactor: refactor codes according to migration doc

--- index.js
+++ index.js
@@ -1,16 +1,40 @@
+const {
+  aceVimMap,
+  mapkey,
+  imap,
+  imapkey,
+  getClickableElements,
+  vmapkey,
+  map,
+  vmap,
+  unmap,
+  iunmap,
+  vunmap,
+  cmap,
+  addSearchAlias,
+  removeSearchAlias,
+  tabOpenLink,
+  readText,
+  Clipboard,
+  Front,
+  Hints,
+  Visual,
+  RUNTIME,
+} = api;
+
 // an example to create a new mapping `ctrl-y`
-api.mapkey("<Ctrl-y>", "Show me the money", function () {
+mapkey("<Ctrl-y>", "Show me the money", function () {
   Front.showPopup(
     "a well-known phrase uttered by characters in the 1996 film Jerry Maguire (Escape to close)."
   );
 });
 
-api.mapkey("<Ctrl-x>", "Remove Ad", function () {
+mapkey("<Ctrl-x>", "Remove Ad", function () {
   const adEl = document.getElementById("ad_topslot");
   adEl.remove();
 });
 
-api.mapkey("<Meta-Shift-Enter>", "Open post-tweet", function () {
+mapkey("<Meta-Shift-Enter>", "Open post-tweet", function () {
   const selectedText = window.getSelection().toString();
   location.href = `post-tweet://?url=${encodeURIComponent(
     window.top.location.href
@@ -20,113 +44,128 @@ api.mapkey("<Meta-Shift-Enter>", "Open post-tweet", function () {
 });
 
 // an example to replace `T` with `gt`, click `Default mappings` to see how `T` works.
-api.map("gt", "T");
+map("gt", "T");
 
 // an example to remove mapkey `Ctrl-i`
-api.unmap("<Ctrl-i>");
+unmap("<Ctrl-i>");
 
 // Navigation
-api.map("<Ctrl-a>", "0");
-api.unmap("0");
-api.map("<Alt-v>", "e");
-api.unmap("e");
-api.map("<Ctrl-v>", "d");
-api.map("<Alt-<>", "gg");
-api.unmap("gg");
-api.map("<Alt->>", "G");
-api.unmap("G");
-api.map("<Ctrl-n>", "j");
-api.map("<Ctrl-p>", "k");
-api.map("<Ctrl-f>", "l");
-api.map("<Ctrl-b>", "h");
-api.map("<Ctrl-e>", "$");
-api.unmap("$");
+map("<Ctrl-a>", "0");
+unmap("0");
+map("<Alt-v>", "e");
+unmap("e");
+map("<Ctrl-v>", "d");
+map("<Alt-<>", "gg");
+unmap("gg");
+map("<Alt->>", "G");
+unmap("G");
+map("<Ctrl-n>", "j");
+map("<Ctrl-p>", "k");
+map("<Ctrl-f>", "l");
+map("<Ctrl-b>", "h");
+map("<Ctrl-e>", "$");
+unmap("$");
 
 // Tabs
-api.map("h", "E");
-api.map("l", "R");
-api.unmap("t");
+map("h", "E");
+map("l", "R");
+unmap("t");
 
 // History
-api.map("B", "S");
-api.unmap("S");
-api.map("F", "D");
-api.unmap("D");
+map("B", "S");
+unmap("S");
+map("F", "D");
+unmap("D");
 
 // Reload
-api.map("R", "r");
-api.unmap("r");
+map("R", "r");
+unmap("r");
 
 // Disable insert mode binding because it's handled by Gtk+
-api.iunmap("<Ctrl-e>");
-api.iunmap("<Ctrl-f>");
-api.iunmap("<Ctrl-u>");
-api.iunmap("<Alt-b>");
-api.iunmap("<Alt-f>");
-api.iunmap("<Alt-d>");
-api.iunmap("<Alt-w>");
+iunmap("<Ctrl-e>");
+iunmap("<Ctrl-f>");
+iunmap("<Ctrl-u>");
+iunmap("<Alt-b>");
+iunmap("<Alt-f>");
+iunmap("<Alt-d>");
+iunmap("<Alt-w>");
 
 // visual mode navigation
-// vmap('<Ctrl-f>', 'l');
-// vmap('<Ctrl-b>', 'h');
-// vmap('<Ctrl-n>', 'j');
-// vmap('<Ctrl-p>', 'k');
-// vmap('<Ctrl-e>', '$');
-// vmap('<Ctrl-a>', '0');
-// vmap('<Alt-f>', 'w');
-// vmap('<Alt-b>', 'b');
-// vmap('<Alt->>', 'G');
-// vmap('<Alt-<>', 'gg');
-// vmap('<Ctrl-l>', 'zz');
+vmap("<Ctrl-f>", "l");
+vmap("<Ctrl-b>", "h");
+vmap("<Ctrl-n>", "j");
+vmap("<Ctrl-p>", "k");
+vmap("<Ctrl-e>", "$");
+vmap("<Ctrl-a>", "0");
+vmap("<Alt-f>", "w");
+vmap("<Alt-b>", "b");
+vmap("<Alt->>", "G");
+vmap("<Alt-<>", "gg");
+vmap("<Ctrl-l>", "zz");
 
 settings.smoothScroll = false;
 
 // Search settings
-// addSearchAliasX('D', 'ddgH', 'https://duckduckgo.com/html/?q=', 's', 'https://duckduckgo.com/ac/?q=', function(response) {
-//     var res = JSON.parse(response.text);
-//     return res.map(function(r){
-//         return r.phrase;
-//     });
-// });
+addSearchAlias(
+  "D",
+  "ddgH",
+  "https://duckduckgo.com/html/?q=",
+  "s",
+  "https://duckduckgo.com/ac/?q=",
+  function (response) {
+    var res = JSON.parse(response.text);
+    return res.map(function (r) {
+      return r.phrase;
+    });
+  }
+);
 settings.defaultSearchEngine = "D";
 
 // Hints settings
-// Hints.characters = "asdfghjkl";
-// Hints.scrollKeys = "";
+Hints.characters = "asdfghjkl";
+Hints.scrollKeys = "";
 settings.hintAlign = "left";
 
-// (function() {
-//     /// Add hooks to Hints so that we can modify Hints.characters ad-hoc.
-//     var orig = {
-//         // create: Hints.create,
-//         // exit: Hints.exit
-//     };
-//     var default_characters = null;
-//     Hints.create = function(_x, _y, attrs) {
-//         if(attrs !== null && typeof(attrs) === "object" && ("debugitos_characters" in attrs)) {
-//             // default_characters = Hints.characters;
-//             // Hints.characters = attrs["debugitos_characters"];
-//         }
-//         orig.create.apply(this, arguments);
-//     };
-//     Hints.exit = function() {
-//         if(default_characters !== null) {
-//             // Hints.characters = default_characters;
-//             default_characters = null;
-//         }
-//         orig.exit.apply(this, arguments);
-//     };
-// }());
-//
-// (function() {
-//     var nums = "456";
-//     mapkey('^f', "#1Open a link (for numpad)", function () {
-//         // Hints.create("", Hints.dispatchMouseClick, {debugitos_characters: nums});
-//     });
-//     mapkey('^C', '#1Open a link in non-active new tab (for numpad)', function() {
-//         // Hints.create("", Hints.dispatchMouseClick, {tabbed: true, active: false, debugitos_characters: nums});
-//     });
-// }());
+(function () {
+  /// Add hooks to Hints so that we can modify Hints.characters ad-hoc.
+  var orig = {
+    create: Hints.create,
+    exit: Hints.exit,
+  };
+  var default_characters = null;
+  Hints.create = function (_x, _y, attrs) {
+    if (
+      attrs !== null &&
+      typeof attrs === "object" &&
+      "debugitos_characters" in attrs
+    ) {
+      default_characters = Hints.characters;
+      Hints.characters = attrs["debugitos_characters"];
+    }
+    orig.create.apply(this, arguments);
+  };
+  Hints.exit = function () {
+    if (default_characters !== null) {
+      Hints.characters = default_characters;
+      default_characters = null;
+    }
+    orig.exit.apply(this, arguments);
+  };
+})();
+
+(function () {
+  var nums = "456";
+  mapkey("^f", "#1Open a link (for numpad)", function () {
+    Hints.create("", Hints.dispatchMouseClick, { debugitos_characters: nums });
+  });
+  mapkey("^C", "#1Open a link in non-active new tab (for numpad)", function () {
+    Hints.create("", Hints.dispatchMouseClick, {
+      tabbed: true,
+      active: false,
+      debugitos_characters: nums,
+    });
+  });
+})();
 
 // set theme
 settings.theme = `
